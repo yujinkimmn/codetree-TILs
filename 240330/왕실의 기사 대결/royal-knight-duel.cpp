db5fcdafee62619adff15409984ddd99129e0673
Yujin Kim <75295578+yujinkimmn@users.noreply.github.com>
2024-03-30 15:16:50 +0900

    Title: 왕실의 기사 대결, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240330/왕실의 기사 대결/royal-knight-duel.cpp
+++ 240330/왕실의 기사 대결/royal-knight-duel.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include<cstring>
 #include <queue>
+#include<cstring>
 
 using namespace std;
 
@@ -10,16 +10,13 @@ using namespace std;
 int L, N, Q;
 
 int info[MAX_L][MAX_L];      // 체스판 상태
-int tmp[MAX_L][MAX_L];  // 디버깅으로 동작 확인용
-int knight_map[MAX_L][MAX_L];   // 기사 영역
 int damage[MAX_N];      // 기사들이 받은 대미지 수
 int first_hp[MAX_N];            // 기사들의 현재 체력
 int hp[MAX_N];            // 기사들의 현재 체력
 int is_moved[MAX_N];    // 이동 여부
 
-pair<int, int> knight_pos[MAX_N];  // 기사들의 위치
-pair<int, int> tmp_knight_pos[MAX_N]; 
-
+pair<int, int> pos[MAX_N];  // 기사들의 위치
+pair<int, int> tmp_pos[MAX_N]; 
 pair<int, int> knight_h_w[MAX_N];   // 기사들의 방패 크기 
 
 int dx[] = {-1, 0, 1, 0};   // 위, 오, 아래, 왼
@@ -30,30 +27,30 @@ int dy[] = {0, 1, 0, -1};
 bool CheckIfMove(int start_num, int dir){
     // 초기화
     for(int i = 1; i <= N ; i++){
-        tmp_knight_pos[i] = knight_pos[i];
         is_moved[i] = false;
         damage[i] = 0;
+        tmp_pos[i] = pos[i];
     }
+    
+    queue<int> qu; 
 
-    // 시작 기사 넣기
-    queue<int> crashed_knights;     
-    crashed_knights.push(start_num);
+    qu.push(start_num);
     is_moved[start_num] = true;
 
     // 연쇄 이동 중에 벽에 부딪히지 않는지 확인하기 -> 모든 이동 무산
-    while(!crashed_knights.empty()){
-        int cur_num = crashed_knights.front();
-        crashed_knights.pop();
+    while(!qu.empty()){
+        int cur_num = qu.front();
+        qu.pop();
 
-        tmp_knight_pos[cur_num].first += dx[dir];
-        tmp_knight_pos[cur_num].second += dy[dir];
+        tmp_pos[cur_num].first += dx[dir];
+        tmp_pos[cur_num].second += dy[dir];
 
-        int new_x = tmp_knight_pos[cur_num].first;
-        int new_y = tmp_knight_pos[cur_num].second;
+        int new_x = tmp_pos[cur_num].first;
+        int new_y = tmp_pos[cur_num].second;
         int h =  knight_h_w[cur_num].first;
         int w =  knight_h_w[cur_num].second;
 
-        // 영역에서 충돌이 발생하는지 확인하기
+        // 함정 & 벽 체크
         for(int i = new_x; i < new_x + h; i++){
             for(int j = new_y; j < new_y + w; j++){
                 
@@ -65,16 +62,29 @@ bool CheckIfMove(int start_num, int dir){
                 if (info[i][j] == 1) {
                     damage[cur_num] += 1;
                 }
-
-                // 자기 자신이 아닌 또 다른 기사와 충돌
-                int other_knight_num = knight_map[i][j];
-                if (other_knight_num > 0 && other_knight_num != cur_num){
-                    if (!is_moved[other_knight_num]){
-                        crashed_knights.push(other_knight_num);
-                        is_moved[other_knight_num] = true;
-                    }
-                }
             }
+        }
+
+        // 다른 기사와 충돌하는지 확인
+        for(int i = 1 ; i <= N; i++){
+
+            // 이미 충돌한 기사거나, 죽은 기사는 패스 
+            if(is_moved[i] || hp[i] <= 0) continue;
+
+            // 겹치지 않는 경우를 따져서 패스 
+            // 상하가 겹치지 않는 경우
+            // 1. 다른 기사(pos[i].first)의 윗변보다 해당 기사의 아랫변(new_x + h - 1)이 더 위에 있거나
+            // 2. 다른 기사의 아랫변보다 해당 기사의 윗변이 더 아래에 있는 경우 
+            if (pos[i].first > new_x + h - 1 || pos[i].first + knight_h_w[i].first - 1 < new_x) continue;
+            
+            // 좌우가 겹치지 않는 경우
+            // 1. 다른 기사의 좌측 변보다 해당 기사의 우측변이 더 작은 경우
+            // 2. 다른 기사의 우측변보다 해당 기사의 좌측 변이 더 큰 경우 
+            if (pos[i].second > new_y + w - 1 || pos[i].second + knight_h_w[i].second - 1 < new_y) continue;
+
+            // 겹치면 큐에 넣기
+            is_moved[i] = true;
+            qu.push(i);
         }
     }
     damage[start_num] = 0;
@@ -83,29 +93,10 @@ bool CheckIfMove(int start_num, int dir){
 
 
 void MoveKnights(){
-    // 기사 지도 초기화
-    memset(knight_map, 0, sizeof(knight_map));
 
     for(int i = 1; i <= N ; i++){
-        // 이동한 기사는 새로운 위치 & 대미지 업데이트
-        if(is_moved[i]){
-            knight_pos[i] = tmp_knight_pos[i];
-            hp[i] -= damage[i];
-        }
-
-        // 죽은 기사는 격자에서 없애기
-        if (hp[i] <= 0) continue;
-
-        int x = knight_pos[i].first;
-        int y = knight_pos[i].second;
-        int h = knight_h_w[i].first;
-        int w = knight_h_w[i].second;
-
-        for(int r = x ; r < x + h; r++){
-            for(int c = y ; c < y + w; c++){
-                knight_map[r][c] = i;
-            }
-        }
+        pos[i] = tmp_pos[i];
+        hp[i] -= damage[i];
     }
 }
 
@@ -123,15 +114,10 @@ int main(){
     for(int i = 1; i <= N; i++){
         int r, c, h, w, k;
         cin >> r >> c >> h >> w >> k;
-        knight_pos[i] = {r, c};
+        pos[i] = {r, c};
         knight_h_w[i] = {h, w};
         hp[i] = k;
         first_hp[i] = k;
-        for(int x = r; x < r + h ; x++){
-            for(int y = c; y < c + w; y++){
-                knight_map[x][y] = i;
-            }
-        }
     }
 
     // Q개의 명령
@@ -154,5 +140,4 @@ int main(){
     }
 
     cout << ans;
-    return 0;
 }
