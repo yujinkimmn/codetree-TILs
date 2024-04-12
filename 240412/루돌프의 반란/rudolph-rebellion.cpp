0f8c580f03297b1507df5080d346c816835e8c48
Yujin Kim <75295578+yujinkimmn@users.noreply.github.com>
2024-04-12 21:46:42 +0900

    Title: 루돌프의 반란, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240412/루돌프의 반란/rudolph-rebellion.cpp
+++ 240412/루돌프의 반란/rudolph-rebellion.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<cmath>
 #include<tuple>
+#include<queue>
 
 using namespace std;
 
@@ -41,14 +42,48 @@ int InRange(int x, int y){
     return 1 <= x && x <= N && 1 <= y && y <= N;
 }
 
+// 산타를 실제로 이동시키는 명령
+// s_num번 산타를 (x, y)에서 dir 방향으로 amount 만큼 이동
+void Move(int s_num, int x, int y, int dir, int amount){
+
+    queue<tuple<int, int, int, int>> qu;
+    qu.push(make_tuple(s_num, x, y, amount));
+
+    while(!qu.empty()){
+        int num, r, c, a; 
+        tie(num, r, c, a) = qu.front(); qu.pop();
+        int nx = r + a * dx[dir];
+        int ny = c + a * dy[dir];
+
+        // 밖으로 튕겨나감 -> 탈락
+        if (!InRange(nx, ny)){
+            dead[num] = true;
+            return;   
+        }
+        // 빈 칸이면 그냥 이동
+        else if(grid[nx][ny] == 0){
+            grid[nx][ny] = num;
+            santa_pos[num] = make_pair(nx, ny);
+            return;
+        }
+        // 다른 산타와 충돌 => 큐로 연쇄 충돌 처리
+        else{
+            // 다른 산타는 1칸 이동
+            qu.push(make_tuple(grid[nx][ny], nx, ny, 1)); 
+            // 자기 자신은 위치 업데이트
+            grid[nx][ny] = num;
+            santa_pos[num] = make_pair(nx, ny);   
+        }
+    }
+}
+
 void RudolfMove(){
     // 가장 가까운 산타 찾기
-    // 거리 작은 순 -> r 큰 순 -> c 큰 순
-    // tuple<int, int, int> closest_santa = make_tuple(1e9, 0, 0); // (거리, r, c)
     int closest_index = 0;
     int closest_dist = 1e9;
 
     // 탈락하지 않은 산타 중 가장 가까운 산타 찾기
+    // 거리 작은 순 -> r 큰 순 -> c 큰 순
     for(int i = N; i >= 1; i--){
         for(int j = N; j >= 1; j--){
             int s = grid[i][j];
@@ -62,18 +97,7 @@ void RudolfMove(){
             }
         }
     }
-    // for(int i = 1 ; i <= P ; i++){
-    //     if(dead[i]) continue;
-    //     int x, y; tie(x, y) = santa_pos[i];
 
-    //     int dist = (int) pow(rx - x, 2) + (int) pow(ry - y, 2);
-    //     tuple<int, int, int> cur = make_tuple(dist, -x, -y);
-
-    //     if(cur < closest_santa){
-    //         closest_santa = cur;
-    //         closest_index = i;
-    //     }
-    // }
     // 이동할 산타가 더 이상 없으면
     if(closest_index == 0) return;
 
@@ -93,60 +117,23 @@ void RudolfMove(){
             dir = d;
         }
     }
-    if(dir == -1) cout << "루돌프 이동 방향 오류\n";
 
-    // 원래 있던 자리는 0으로
+    // 원래 있던 자리는 0으로(루돌프는 무조건 해당 자리로 이동한다)
     grid[rx][ry] = 0;
     // 루돌프 새로운 위치로 이동
     tie(rx, ry) = make_pair(rx + dx[dir], ry + dy[dir]);
-
-    // 산타와 충돌했다면 충돌 처리
-    if(grid[rx][ry] > 0){
-        int s1 = grid[rx][ry];
-        // turn+1 까지 기절 
-        sleep[s1] = turn + 1;
-        // C만큼 점수 추가
-        scores[s1] += C;
-        // s1 산타는 루돌프의 이동방향 dir으로 C칸 움직임
-        int nx = rx + C * dx[dir], ny = ry + C * dy[dir];
-
-        // 격자 밖이면 탈락
-        if(!InRange(nx, ny)){
-            dead[s1] = true;
-        }
-
-        // 산타가 움직인 칸에 다른 산타가 있으면 연쇄 충돌 -> 연쇄 충돌 일어나는 마지막 산타 찾기
-        else if(grid[nx][ny] > 0){
-            int end_x = nx, end_y = ny;
-            // 격자 내에서 다른 산타가 있는 동안은 계속 이동
-            while(InRange(end_x, end_y) && grid[end_x][end_y] > 0){
-                end_x += dx[dir], end_y += dy[dir];
-            }
-            // 마지막 칸에 이전 산타들 하나씩 넣어주기 
-            while(end_x != nx || end_y != ny){
-                int before_santa = grid[end_x - dx[dir]][end_y - dy[dir]];
-                if (InRange(end_x, end_y)){
-                    grid[end_x][end_y] = before_santa;
-                    santa_pos[before_santa] = {end_x, end_y};
-                }
-                else 
-                    dead[before_santa] = true;
-                end_x -= dx[dir], end_y -= dy[dir];
-            }
-            // 연쇄충돌 처음 일어난 산타 넣어주기
-            grid[nx][ny] = s1;
-            santa_pos[s1] = make_pair(nx, ny);
-        }
-
-        // 빈 칸이면 그냥 이동
-        else{
-            // 원래 있던 칸은 루돌프가 채울거임
-            grid[nx][ny] = s1;
-            santa_pos[s1] = make_pair(nx, ny);
-        }
-    }
-    // 산타 모두 처리 후 격자에서 루돌프 위치 이동
+    // 격자에서 루돌프 위치 이동
     grid[rx][ry] = -1;
+
+    // 루돌프가 이동한 위치에서 산타와 충돌 => 산타 이동 
+    if(make_pair(rx, ry) == make_pair(target_x, target_y)){
+        int s = closest_index;
+        // 산타 C점 얻고, turn + 1까지 기절하고, 루돌프 이동 방향으로 C칸
+        scores[s] += C;
+        sleep[s] = turn + 1;
+        // 산타 있던 자리는 어짜피 루돌프가 차지함
+        Move(s, rx, ry, dir, C);    // 산타 s가 (rx, ry)에서 dir 방향으로 C칸 이동
+    }
 }
 
 
@@ -178,66 +165,45 @@ void SantaMove(){
 
         // 이동 가능한 위치
         pair<int, int> new_pos = make_pair(sx + dx[dir], sy + dy[dir]);
-        
-        // 루돌프랑 충돌 없으면 그냥 이동(산타랑 충돌은 X. 어짜피 산타 있는 칸 못감)
-        if(make_pair(rx, ry) != new_pos) {
-            // 원래 있던 칸 초기화
-            grid[sx][sy] = 0;
+        // 원래 자리는 0으로
+        grid[sx][sy] = 0;
+
+        // 루돌프와 충돌했는지?
+        if(new_pos == make_pair(rx, ry)){
+            // D점 획득, turn+1까지 기절
+            scores[i] += D;
+            sleep[i] = turn + 1;
+
+            // 산타를 자신의 이동방향 반대로 D칸 이동
+            Move(i, new_pos.first, new_pos.second, (dir+2)%4, D);
+        }
+
+        // 충돌 안했으면 그 자리 그대로 이동
+        else{   
+            // 새로운 자리로 업데이트
             grid[new_pos.first][new_pos.second] = i;
             santa_pos[i] = new_pos;
         }
-
-        // 루돌프가 있으면 충돌
-        else {
-            // 해당 산타는 D점 얻기
-            scores[i] += D;
-            // turn + 1까지 기절
-            sleep[i] = turn + 1;
-
-            // D == 1이면 산타는 원래 자기 자리를 유지한다. 
-            if(D == 1) continue;
-            
-            // 아니면 산타는 자신의 이동방향 반대로 D칸 이동
-            dir = (dir + 2) % 4;
-            int nx = rx + D * dx[dir], ny = ry + D * dy[dir];
-
-            // 격자 밖이면 원래 있던 칸 초기화만 하고 탈락
-            if (!InRange(nx, ny)){
-                dead[i] = true;
-                grid[sx][sy] = 0;
-            }
-            // (nx, ny)에 다른 산타가 있으면 연쇄 충돌 -> 연쇄 충돌 일어나는 마지막 산타 찾기
-            else {
-                int end_x = nx, end_y = ny;
-                // 격자 내에서 다른 산타가 있는 동안은 계속 이동
-                while(InRange(end_x, end_y) && grid[end_x][end_y] > 0){
-                    end_x += dx[dir], end_y += dy[dir];
-                }
-                // 마지막 칸에 이전 산타들 하나씩 넣어주기 
-                while(end_x != nx || end_y != ny){
-                    int before_santa = grid[end_x - dx[dir]][end_y - dy[dir]];
-                    if (InRange(end_x, end_y)){
-                        grid[end_x][end_y] = before_santa;
-                        santa_pos[before_santa] = {end_x, end_y};
-                    }
-                    else 
-                        dead[before_santa] = true;
-                    end_x -= dx[dir], end_y -= dy[dir];
-                }
-                // 연쇄충돌 처음 일어난 산타 처리
-                grid[nx][ny] = i;
-                santa_pos[i] = {nx, ny};
-                // 원래 있던 자리는 0으로
-                grid[sx][sy] = 0;
-            }
-        }
     }
 }
+
+
 
 void AddScore(){
     // 살아남은 산타들 점수 추가
     for(int i = 1; i <= P; i++){
         if(!dead[i]) scores[i] += 1;
+    }
+}
+
+void print(){
+    // 디버깅용
+    cout << "\n";
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= N; j++){
+            cout << grid[i][j] << " ";
+        }
+        cout << "\n";
     }
 }
 
@@ -259,13 +225,14 @@ int main(){
 
         // Step 1. 루돌프 움직임 1칸
         RudolfMove();
+        //print();
 
         // Step 2. 산타 움직임
         SantaMove();
+        //print();
 
         // 살아 있는 산타들은 1점씩 추가하기
         AddScore();
-
     }
 
     // 산타들이 얻은 점수 차례로 출력
